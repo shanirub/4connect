8cb5dbfdf7a0ea0c7e43bb16291e7888eac10ecc
srub <shani.rubovitch@gmail.com>
2021-01-09 17:03:41 +0100

    ...

---
+++ network/server.py
@@ -0,0 +1,26 @@
+from enum import Enum
+import socketio
+import uvicorn
+
+
+class OpCode(Enum):
+    PLAY = 0
+    WIN = 1
+    LOSS = 2
+
+
+def find_op_code():
+    pass
+
+
+def initiate_game():
+    pass
+
+
+sio = socketio.AsyncServer(async_mode='asgi')
+app = socketio.ASGIApp(sio)
+
+if __name__ == '__main__':
+    # create a Socket.IO server
+
+    uvicorn.run("server:app", host="127.0.0.1", port=5000, log_level="info")
