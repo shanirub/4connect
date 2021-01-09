06e6e1c902122ce78b2e88b8e0b0e01e4db5ced9
srub <shani.rubovitch@gmail.com>
2021-01-09 17:40:46 +0100

    now with await

--- network/client.py
+++ network/client.py
@@ -2,18 +2,25 @@ import socketio
 
 sio = socketio.AsyncClient()
 
+
 @sio.event
 def connect():
     print('connection established')
+
 
 @sio.event
 def my_message(data):
     print('message received with ', data)
     sio.emit('my response', {'response': 'my response'})
 
+
 @sio.event
 def disconnect():
     print('disconnected from server')
 
-sio.connect('http://localhost:5000')
-sio.wait()
+
+async def con():
+    await sio.connect('http://localhost:5000')
+
+if __name__ == '__main__':
+    con()
